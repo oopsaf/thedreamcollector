98b80fa0bbe6e16d30d6611329dc30780d168728
oopsaf <71773326+oopsaf@users.noreply.github.com>
2022-06-19 16:00:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-    location.href = 'https://www.figma.com/proto/S480S6OsYvHsr3mM2jh2JV/tdc?node-id=45%3A12';
+    window.location.href = 'https://www.figma.com/proto/S480S6OsYvHsr3mM2jh2JV/tdc?node-id=45%3A12';
 }
